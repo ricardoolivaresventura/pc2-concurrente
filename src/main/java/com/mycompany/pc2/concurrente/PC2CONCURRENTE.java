4e5c1f40f3f28a6b42c6b21b1711f412fa2e9f35
ricardo leonardo olivares ventura <rlov@MacBook-Air-de-ricardo.local>
2023-10-05 22:59:59 -0500

    Create repository

---
+++ src/main/java/com/mycompany/pc2/concurrente/PC2CONCURRENTE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pc2.concurrente;
+
+/**
+ *
+ * @author rlov
+ */
+public class PC2CONCURRENTE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
